28d0db17674693d2c7e98eae34489af375aa6ab3
Dilter Porto <dilter.itix@dacasafin.onmicrosoft.com>
2018-06-10 00:12:40 -0300

    consolidador project added

---
+++ lancamento-service/src/Stone.Lancamento.Consolidador/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Stone.Lancamento.Consolidador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Verbose)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Log.Information("Starting web host");
+            BuildWebHost(args).Run();
+        }
+        
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+    }
+}
---
+++ lancamento-service/src/Stone.Lancamento.Consolidador/Startup.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Stone.Lancamento.Application;
+using Stone.Lancamento.Application.Commands;
+using Stone.Lancamento.Domain.Lancamentos.Services;
+using Stone.Lancamento.Persistence.Configuration;
+using Stone.Lancamento.Persistence.Extensions;
+using Stone.Sdk.Extensions;
+
+namespace Stone.Lancamento.Consolidador
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services
+                .AddPersistenceEfContext<LancamentosDbContext>(Configuration)
+                .AddScoped<ProcessarPagamento>()
+                .AddScoped<ProcessarRecebimento>()
+                .AddRepositories();
+            
+            services
+                .AddMediatR(typeof(CriarLancamentoCommand).Assembly);
+
+            services
+                .AddMessageBroker(Configuration);
+
+            services
+                .AddApplicationMappings();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCommandHandlerFor<ProcessarPagamentoCommand>();
+            app.UseCommandHandlerFor<ProcessarRecebimentoCommand>();
+            app.UseCommandHandlerFor<ConsolidarLancamentosCommand>();
+        }
+    }
+}
